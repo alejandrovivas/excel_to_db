c8cc3f13eaa551d63f047002f1d125e38ed88823
Alejandro Vivas <alejo.vivas.av@gmail.com>
2024-06-14 01:12:55 -0500

    testing aws bucket

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from openpyxl.drawing.image import Image
 import pandas as pd
 import pymysql
 import logging
-import os
-# import io
-# import boto3
-# from botocore.exceptions import NoCredentialsError
+from io import BytesIO
+import boto3
 import re
-from categorias import categorias
+from datetime import datetime
+from categorias import (categorias, HOST, PASSWORD, PORT, USER, DATABASE,AWS_BUCKET_NAME, AWS_DEFAULT_REGION,
+                        AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_IMAGES_PATH)
 
 app = Flask(__name__)
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)  # You can adjust the logging level
-logger = logging.getLogger(__name__)
-
-
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+log_filename = f'excel_{timestamp}.log'
+logging.basicConfig( filename=log_filename, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s',
+                     datefmt='%Y-%m-%d %H:%M:%S')
 
 @app.route('/upload-excel', methods=['POST'])
 def upload_excel():
@@ -28,95 +28,99 @@ def upload_excel():
     if file.filename == '':
         return 'No selected file', 400
 
-    try:
-        # Read the Excel file
-        excel_file = pd.read_excel(file, sheet_name=None)
-        # use openpyxl to get images data
-        temp_file_path = os.path.join('/tmp', file.filename)
-        file.save(temp_file_path)
-        wb = load_workbook(temp_file_path)
+    # try:
+    # # Read the Excel file
+    excel_file = pd.read_excel(file, sheet_name=None)
 
-        # We change to False the active status of each product
-        execute_sql("UPDATE producto SET activo = 0 WHERE activo = 1;")
+    # Load the workbook and the specific sheet with openpyxl
+    wb = load_workbook(file.stream)
 
-        # Iter each excel sheet
-        for sheet_name, dataframe in excel_file.items():
-            #TODO validar categoria en archivo de categorias
-            logger.info(sheet_name)
-            category = category_search(sheet_name)
+    # We change to False the active status of each product
+    execute_sql("UPDATE producto SET activo = 0 WHERE activo = 1;")
 
-            #Get products category
-            category = execute_sql("SELECT id_categoria FROM categoria WHERE nombre=%s", (sheet_name,))
+    # Iter each excel sheet
+    for sheet_name, dataframe in excel_file.items():
+        logging.info(f'Cargando datos: {sheet_name}')
+        #Validate categories on local dictionary
+        category = find_category(sheet_name)
+        if category:
+            # Get products category from database
+            category_db = execute_sql("SELECT id_categoria FROM categoria WHERE nombre=%s", (category,))
 
-            if not category:  # Check if dictionary is empty
-                return f'Error uploading data: there is not category {sheet_name}'
+            # Check if dictionary is empty
+            if not category_db:
+                logging.error(f'Error cargando datos de la categoria {sheet_name}, '
+                              f'no se encontro la categoria en la base de datos')
+                continue
             else:
-                pass
-                # ws = wb[sheet_name]
-                # for index, row in dataframe.iterrows():
-                #     row, validation = validate_data(row)
-                #     if validation:
-                #         # TODO SAVE IMAGES SDFASDFSADASFSDASDDSFASDF.PNG
-                #         #Verify if there is an image on row
-                #         for image in ws._images:
-                #             if image.anchor._from.row == index and image.anchor._from.col == row['Imagen']:
-                #
-                #                 # Nombre del archivo de salida
-                #                 image_filename = os.path.join(output_folder, f"imagen_{index}.png")
-                #
-                #                 # Guarda la imagen
-                #                 image_bytes = image._data
-                #                 image_file = io.BytesIO(image_bytes)
-                #                 with open(image_filename, 'wb') as f:
-                #                     f.write(image_file.read())
-                #
-                #                 print(f"Imagen guardada en: {image_filename}")
-                #
-                #
-                #         cell_coordinate = row['Imagen']
-                #
-                #         query = """INSERT INTO producto (codigo_ean, titulo, descripcion, precio, url_imagen, id_categoria, activo)
-                #                 VALUES (%s, %s, %s, %s, %s, %s %s)"""
-                #         args = (row['CODIGO EAN'], row['REFERENCIA'], row['DESCRIPCION'], row['PRECIO EN ALMACENES DE CADENA'], row['IMAGEN'], category['id_categoria'], True)
-                #         execute_sql(query, args)
-                #     else:
-                #         return f'Error uploading data: there is an error with file: {index} on {sheet_name} sheet'
+                ws = wb[sheet_name]
+                for index, row in dataframe.iterrows():
+                    row, validation = validate_data(row)
+                    cell = ws.cell(row=index, column=8)
+
+                    for image in ws._images:
+                        if image.anchor == cell.coordinate:
+                            save_image_to_s3(image)
+                            return
+
+                    raise ValueError(f"No image found in cell '{cell}'.")
+
+                    # if validation:
+                    #     # TODO SAVE IMAGES SDFASDFSADASFSDASDDSFASDF.PNG
+                    #
+                    #
+                    #     query = """INSERT INTO producto (codigo_ean, titulo, descripcion, precio, url_imagen,
+                    #             id_categoria, activo) VALUES (%s, %s, %s, %s, %s, %s %s)"""
+                    #     args = (row['CODIGO EAN'], row['REFERENCIA'], row['DESCRIPCION'],
+                    #             row['PRECIO EN ALMACENES DE CADENA'], row['IMAGEN'], category['id_categoria'], True)
+                    #     execute_sql(query, args)
+                    # else:
+                    #     return f'Error uploading data: there is an error with file: {index} on {sheet_name} sheet'
             break
+        else:
+            logging.error(f'Error cargando datos de la categoria {sheet_name}, no se encontro la categoria en los archivos locales')
+            continue
+    #
+    #     return 'Data uploaded successfully', 200
+    # except Exception as e:
+    #     return f'Error uploading data: {str(e)}', 500
 
-        return 'Data uploaded successfully', 200
-    except Exception as e:
-        return f'Error uploading data: {str(e)}', 500
-
-def category_search(sheet_name):
+def find_category(search_value):
     for key, values in categorias.items():
-        if sheet_name == values:
-            return key
-    return None  # return None if there is not any category
+        if isinstance(values, list):
+            if search_value in values:
+                return key
+        else:
+            if search_value == values:
+                return key
+    return None
 
-# def upload_to_s3(file_name, bucket, object_name):
-#     # Crear una sesión de S3
-#     s3_client = boto3.client('s3')
-#
-#     try:
-#         # Subir el archivo
-#         s3_client.upload_file(file_name, bucket, object_name)
-#         print(f"Archivo {file_name} subido exitosamente a {bucket}/{object_name}")
-#     except FileNotFoundError:
-#         print("El archivo no fue encontrado")
-#     except NoCredentialsError:
-#         print("Credenciales no disponibles")
-#     except Exception as e:
-#         print(f"Error al subir el archivo: {e}")
 
+def save_image_to_s3(image):
+    # Convert the image to a byte stream
+    img_stream = BytesIO()
+    image.image.save(img_stream, format='PNG')
+    img_stream.seek(0)
+
+    # Initialize a session using boto3
+    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                      region_name=AWS_DEFAULT_REGION)
+
+    # Upload the image to the specified S3 bucket
+    s3.put_object(Bucket=AWS_BUCKET_NAME, Key=AWS_DEFAULT_IMAGES_PATH, Body=img_stream, ContentType='image/png')
+
+    print(f"Image uploaded to S3 bucket '{AWS_BUCKET_NAME}' with key '{AWS_DEFAULT_IMAGES_PATH}'")
 
 def execute_sql(query, args=None):
     # Connect to the database
-    connection = pymysql.connect(host='44.212.212.72', port=3307, user='root',password='Trabajos100@',
-                                 database='creditos',cursorclass=pymysql.cursors.DictCursor)
+    connection = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD,database=DATABASE,
+                                 cursorclass=pymysql.cursors.DictCursor)
 
     with connection.cursor() as cursor:
         # Execute SQL query
         cursor.execute(query, args)
+
         # Return category data if exists
         data = cursor.fetchall()
         if data:
